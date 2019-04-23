7d85865992ec2ad93d6be77e62294faf3a757d6e
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-04-23 13:31:39 +0100

    - tweaks to site setup

--- head-section.php
+++ head-section.php
@@ -1,15 +1,15 @@
 <?php
 	date_default_timezone_set('UTC');
 	// opens DB connexion
-	require ('../../init_ptwin_shop.php');
-	require ('functions.php');
+	require_once('../../init_ptwin_shop.php');
+	require_once('functions.php');
 ?>
 
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- additional options to content to prevent zooming on mobile devices, maximum-scale=1, user-scalable=no"-->
-<title><?php echo $page_title; ?></title>
+<title><?= $page_title; ?></title>
 <script type="text/javascript" src="jQuery/jquery-1.12.3.min.js"></script>
 <script type="text/javascript" src="jQuery/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-<link rel="stylesheet" type="text/css" href="jQuery/jquery-ui-1.11.4/jquery-ui.min.css"/>
-<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" type="text/css" href="jQuery/jquery-ui-1.11.4/jquery-ui.min.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
