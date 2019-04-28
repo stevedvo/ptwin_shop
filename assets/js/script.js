5c333aeedd419c80a22f35c10e6aac604a9d131b
steveDVO <steve.deveaux@blueyonder.co.uk>
2019-04-28 21:27:16 +0100

    - enhancement/#3 - starting to work on the client-side validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,42 +17,42 @@ function mamnageLists()
 		{
 			var listName = form.find("[name='list-name']").val();
 
-			$.ajax(
-			{
-				type     : "POST",
-				url      : "/ajax.php",
-				dataType : "json",
-				data     :
-				{
-					action : "addList",
-					request : {list_name : listName}
-				}
-			}).done(function(data)
-			{
-				if (data)
-				{
-					var html =
-					'<tr>'+
-						'<input type="hidden" name="list-id" value="'+parseInt(data)+'" />'+
-						'<td><input type="submit" name="view-list" value="View" /></td>'+
-						'<td><input type="text" name="list-name" value="'+listName+'" /></td>'+
-						'<td><input type="submit" name="update-list" value="Update" /></td>'+
-					'</tr>';
+			// $.ajax(
+			// {
+			// 	type     : "POST",
+			// 	url      : "/ajax.php",
+			// 	dataType : "json",
+			// 	data     :
+			// 	{
+			// 		action : "addList",
+			// 		request : {list_name : listName}
+			// 	}
+			// }).done(function(data)
+			// {
+			// 	if (data)
+			// 	{
+			// 		var html =
+			// 		'<tr>'+
+			// 			'<input type="hidden" name="list-id" value="'+parseInt(data)+'" />'+
+			// 			'<td><input type="submit" name="view-list" value="View" /></td>'+
+			// 			'<td><input type="text" name="list-name" value="'+listName+'" /></td>'+
+			// 			'<td><input type="submit" name="update-list" value="Update" /></td>'+
+			// 		'</tr>';
 
-					$(".results-table").append(html);
+			// 		$(".results-table").append(html);
 
-					toastr.success("New List successfully added");
+			// 		toastr.success("New List successfully added");
 
-				}
-				else
-				{
-					toastr.error("An error occurred");
-				}
-			}).fail(function(data)
-			{
-				toastr.error("Could not perform request");
-				console.log(data);
-			});
+			// 	}
+			// 	else
+			// 	{
+			// 		toastr.error("An error occurred");
+			// 	}
+			// }).fail(function(data)
+			// {
+			// 	toastr.error("Could not perform request");
+			// 	console.log(data);
+			// });
 		}
 	});
 }
@@ -62,7 +62,17 @@ function validateForm(form)
 	var inputs = form.find("input, select, textarea");
 	var result = {};
 
-	// console.log(inputs);
+	if (inputs.length > 0)
+	{
+		inputsToValidate = [];
+
+		$.each(inputs, function()
+		{
+			
+		});
+	}
+
+	console.log(inputs);
 
 	return result;
 }
