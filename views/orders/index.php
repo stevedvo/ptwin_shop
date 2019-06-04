873619bc9a865014370d1438a1b76e566bace138
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-06-04 13:15:54 +0100

    - enhancement/#14 - complete View for /orders/

--- views/orders/index.php
+++ views/orders/index.php
@@ -1,15 +1,15 @@
 <?php
 	$orders = $response['orders'];
-/*?>
+?>
 <main class="wrapper">
 	<div class="container">
 <?php
-		if (!$order)
+		if (!is_array($orders) || sizeof($orders) == 0)
 		{
 ?>
 			<div class="row">
 				<div class="col-xs-12">
-					<p>Could not find Order / invalid request</p>
+					<p class="no-results">No Orders can be found</p>
 				</div>
 			</div>
 <?php
@@ -19,79 +19,29 @@
 ?>
 			<div class="row">
 				<div class="col-xs-12">
-					<p>Order ID: <?= $order->getId(); ?></p>
-					<p>Date Ordered: <?= $order->getDateOrdered() ? $order->getDateOrdered()->format('d-m-Y') : ""; ?></p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12">
+					<h3>Orders</h3>
 					<div class="results-container striped">
 <?php
-						if (is_array($order->getOrderItems()) && sizeof($order->getOrderItems()) > 0)
-						{
-							$current_dept = null;
-							$i = 0;
-
-							foreach ($order->getOrderItems() as $order_item_id => $order_item)
-							{
-								$item_dept = $order_item->getItem()->getPrimaryDept();
-								$dept_heading = !is_null($item_dept) ? $departments[$item_dept]->getName() : 'No collection defined';
-
-								if ($i == 0)
-								{
-?>
-									<div class="collection-container">
-										<h4><?= $dept_heading; ?></h4>
-										<div class="collection-items-container">
-<?php
-								}
-								elseif ($item_dept != $current_dept)
-								{
-?>
-										</div>
-									</div>
-
-									<div class="collection-container">
-										<h4><?= $dept_heading; ?></h4>
-										<div class="collection-items-container">
-<?php
-								}
-?>
-											<div class="row result-item">
-												<div class="col-xs-2 description-container">
-													<a href="<?= SITEURL; ?>/items/edit/<?= $order_item->getItemId(); ?>/"><p><?= $order_item->getItem()->getDescription(); ?></p></a>
-												</div>
-
-												<div class="col-xs-2 comments-container">
-													<p><?= $order_item->getItem()->getComments(); ?></p>
-												</div>
-
-												<div class="col-xs-7 link-container">
-													<a href="<?= $order_item->getItem()->getLink(); ?>" target="_blank"><p><?= $order_item->getItem()->getLink(); ?></p></a>
-												</div>
-
-												<div class="col-xs-1 quantity-container">
-													<p><?= $order_item->getQuantity(); ?></p>
-												</div>
-											</div>
-<?php
-								$current_dept = $item_dept;
-								$i++;
-
-								if ($i == sizeof($order->getOrderItems()))
-								{
-?>
-										</div>
-									</div>
-<?php
-								}
-							}
-						}
-						else
+						foreach ($orders as $order_id => $order)
 						{
 ?>
-							<p class="no-results">No Items in this Order</p>
+							<div class="row result-item">
+								<div class="col-xs-2 id-container">
+									<p>#<?= $order->getId(); ?></p>
+								</div>
+
+								<div class="col-xs-4 date-ordered-container">
+									<p><?= !is_null($order->getDateOrdered()) ? $order->getDateOrdered()->format('d-m-Y') : ''; ?></p>
+								</div>
+
+								<div class="col-xs-3 button-container">
+									<a href="<?= SITEURL.'/orders/view/'.$order->getId().'/'; ?>" class="btn btn-sm btn-primary pull-right">View</a>
+								</div>
+
+								<div class="col-xs-3 button-container">
+									<a href="<?= SITEURL.(!is_null($order->getDateOrdered()) ? '/orders/edit/'.$order->getId().'/' : '/'); ?>" class="btn btn-sm btn-danger pull-right">Edit</a>
+								</div>
+							</div>
 <?php
 						}
 ?>
@@ -103,4 +53,3 @@
 ?>
 	</div>
 </main>
-*/
