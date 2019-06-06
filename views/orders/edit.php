655445317c24872b2499173fc6c8cd3845f03151
Steve DeVeaux <steve.deveaux@kloc.co.uk>
2019-06-06 14:19:30 +0100

    - enhancement/#14 - amend /orders/edit/{id}/ View to allow each OrderItem to be amended or removed

--- views/orders/edit.php
+++ views/orders/edit.php
@@ -30,7 +30,7 @@
 
 			<div class="row">
 				<div class="col-xs-12">
-					<div class="results-container striped">
+					<div class="results-container previous-order striped">
 <?php
 						if (is_array($order->getOrderItems()) && sizeof($order->getOrderItems()) > 0)
 						{
@@ -62,21 +62,21 @@
 <?php
 								}
 ?>
-											<div class="row result-item">
-												<div class="col-xs-2 description-container">
-													<a href="<?= SITEURL; ?>/items/edit/<?= $order_item->getItemId(); ?>/"><p><?= $order_item->getItem()->getDescription(); ?></p></a>
+											<div class="row form result-item" data-order_item_id="<?= $order_item->getId(); ?>">
+												<div class="col-xs-8 description-container">
+													<p><a href="<?= SITEURL; ?>/items/edit/<?= $order_item->getItemId(); ?>/"><?= $order_item->getItem()->getDescription(); ?></a></p>
 												</div>
 
-												<div class="col-xs-2 comments-container">
-													<p><?= $order_item->getItem()->getComments(); ?></p>
+												<div class="col-xs-4 quantity-container">
+													<input type="number" name="quantity" data-validation="<?= $order_item->getValidation("Quantity"); ?>" value="<?= $order_item->getQuantity(); ?>" />
 												</div>
 
-												<div class="col-xs-7 link-container">
-													<a href="<?= $order_item->getItem()->getLink(); ?>" target="_blank"><p><?= $order_item->getItem()->getLink(); ?></p></a>
+												<div class="col-xs-4 col-xs-offset-4 update button-container">
+													<button class="btn btn-sm btn-primary pull-right js-update-order-item">Update</button>
 												</div>
 
-												<div class="col-xs-1 quantity-container">
-													<p><?= $order_item->getQuantity(); ?></p>
+												<div class="col-xs-4 remove button-container">
+													<button class="btn btn-sm btn-danger pull-right js-remove-order-item">Remove</button>
 												</div>
 											</div>
 <?php
