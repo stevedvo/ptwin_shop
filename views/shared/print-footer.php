6baa1db351613f3309a87cf1ff9138c79792e61f
steveDVO <steve.deveaux@kloc.co.uk>
2020-04-11 22:15:53 +0100

    - enhancement/#42 - remove commented-out code

--- views/shared/print-footer.php
+++ views/shared/print-footer.php
@@ -1,13 +1,4 @@
 		<footer>
-<?php
-			// $script =
-			// '<script type="text/javascript">'.
-			// 	'constants = {"SITEURL" : "'.SITEURL.'"};'.
-			// '</script>';
-
-			// echo $script;
-?>
-			<!-- <script type="text/javascript" src="<?= SITEURL; ?>/assets/js/script.js"></script> -->
 		</footer>
 	</body>
 </html>
